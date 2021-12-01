8f78045d0e171710c45cf70a4fe13e9ce5d697b3
Bryan Herbst <bryan.herbst@target.com>
2021-12-01 08:43:39 -0600

    Day 1 done!

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,22 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.map { it.toInt() }
+            .windowed( size = 2, step = 1)
+            .count { (first, second) -> second > first }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.map { it.toInt() }
+            .windowed(size = 3, step = 1 )
+            .map { it.sum() }
+            .windowed(size = 2, step = 1)
+            .count { (first, second) -> second > first }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testResult = (part1(testInput) == 7)
+    println("test passed: $testResult")
 
     val input = readInput("Day01")
     println(part1(input))
